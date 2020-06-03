3e61ebc0a95b3022e92ef3a9cd77b4886d099c2e
bussy-lere <oluwabusayomi123@gmail.com>
2020-06-03 13:25:57 +0100

    Update payments.dart

--- lib/db/payments.dart
+++ lib/db/payments.dart
@@ -3,7 +3,7 @@ import 'package:stripe_payment/stripe_payment.dart';
 class PaymentService{
   PaymentService(){
     StripePayment.setOptions(
-      StripeOptions(publishableKey: 'pk_test_9IO9My8W3b8FKrZQz392kiQG00enHFmKTW')
+      StripeOptions(publishableKey: '')
     );
   }
 }
